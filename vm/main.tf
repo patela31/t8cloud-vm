af3b6d99860173f267b5af8d97021a243754d356
Attila Patel <attila_patel@hotmail.com>
2021-11-15 18:16:28 +0000

    test

--- vm/main.tf
+++ vm/main.tf
@@ -111,13 +111,6 @@ resource "azurerm_storage_account" "mystorageaccount" {
     }
 }
 
-# Create (and display) an SSH key
-resource "tls_private_key" "example_ssh" {
-  algorithm = "RSA"
-  rsa_bits = 4096
-}
-output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
-
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
     name                  = "atulvm-vmlinuxt8"
